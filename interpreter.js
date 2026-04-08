c2d7177d8dc3e301c44e946d7d0ba407e64aafc9
Pretzels <117748267+SuckyPretzels@users.noreply.github.com>
2026-04-07 23:40:16 -0300

    more updates to the interpreter! working towards adding arithmetic. things are very broken and untested right now

--- interpreter.js
+++ interpreter.js
@@ -1,19 +1,22 @@
 run(
     `print "Hello World"
-print "i love you"
-let x = 5
-print x`
-  
+print 5
+print 2 3 +
+let x = 10
+print x
+` 
 );
 
 function run(code) {
     const tokens = tokenizer(code);
     const ast = parser(tokens);
-    execute(ast);
+    console.log(ast);
+    // execute(ast);
 }
 
 function tokenizer(code) {
     const keywords = new Set(["print","let"]);
+    const operators = new Set(["+","-","*","/","="]);
     // this creates a new Set() object. Sets are not like arrays, they are lookup tables.
 
     function getType(value) {
@@ -26,7 +29,7 @@ function tokenizer(code) {
         if (/^".*"$/.test(value)) {
             return "string";
         }
-        if (value === "=") {
+        if (operators.has(value)) {
             return "operator";
         }
         return "identifier";
@@ -51,7 +54,7 @@ function tokenizer(code) {
         if (char === '"') {
             inQuotes = !inQuotes;
             current += char;
-        } else if ((char === " " || char === "\n" || char === "=") && !inQuotes) {
+        } else if ((char === " " || char === "\n" || operators.has(char)) && !inQuotes) {
             if (current.length > 0) {
                 tokens.push({
                     type: getType(current),
@@ -61,7 +64,7 @@ function tokenizer(code) {
                 });
                 current = "";
             }
-            if (char === "=") {
+            if (operators.has(char)) {
                 tokens.push({
                     type: getType(char), 
                     value: char,
@@ -93,7 +96,6 @@ function tokenizer(code) {
     return tokens;
 }
 
-
 function parser(tokens) {
     const ast = [];
     let i = 0;
@@ -101,73 +103,179 @@ function parser(tokens) {
     while (i < tokens.length) {
         const token = tokens[i];
 
-        if (token.type === "keyword" && token.value === "print") {
-            if (i+1 < tokens.length) {
-                const next = tokens[i+1];
-                let value = next.value;
-
-                if (next.type === "string") {
-                    value = value.replace(/^"|"$/g, "");
-                }
-
-                ast.push({
-                    type: "print", 
-                    value: value,
-                    isLiteral: next.type === "string"
-                });
+        if (token.type === "keyword") {
+            let result = parseKeyword(tokens, i);
+            if (result && result.node) {
+                ast.push(result.node);
+                i+=result.consumed;
+            } else {
+                i++;
             }
-            i+=2;
-        }
-        else if (token.type === "keyword" && token.value === "let") {
-            if (i+3 < tokens.length) {
-                const nameToken = tokens[i+1];
-                const opToken = tokens[i+2];
-                const valueToken = tokens[i+3];
-
-                if (nameToken.type === "identifier" && opToken.value === "=") {
-                    let storedValue = valueToken.value;
-
-                    if (valueToken.type === "string") {
-                        storedValue = storedValue.replace(/^"|"$/g, "");
-                    }
-
-                    ast.push({
-                        type: "let",
-                        name: nameToken.value,
-                        value: storedValue,
-                        valueType: valueToken.type
-                    });
-                }
-            }
-            i+=4;
-        }
-        else {
+        } else {
             i++;
         }
     }
     return ast;
 }
+function parseKeyword(tokens, index) {
+    const token = tokens[index];
+    switch (token.value) {
+    case "print":
+        return parsePrint(tokens, index);
+
+    case "let":
+        return parseLet(tokens, index);
+
+    default:
+        return none;
+    }
+
+    function parsePrint(tokens, index) {
+        if (index+1 >= tokens.length) {
+            return null;
+        } else {
+            const result = parseExpression(tokens, index+1);
+
+            if (!result) {
+                return null;
+            }
+
+            return {
+                node: {
+                    type: "print", 
+                    expression: result.node,
+                },
+                consumed: 1 + result.consumed
+            };
+        }
+    }
+    function parseLet(tokens, index) {
+        if (index+3 >= tokens.length) {
+            return null;
+        } else {
+            const nameToken = tokens[index+1];
+            const opToken = tokens[index+2];
+            
+            if (nameToken.type !== "identifier" || opToken.value !== "=") {
+                return null;
+            }
+
+            const result = parseExpression(tokens, index+3);
+            if (!result) {
+                return null;
+            }
+            return {
+                node: {
+                    type: "let",
+                    name: nameToken.value,
+                    expression: result.node
+                },
+                consumed: 3 + result.consumed
+            };
+        }
+    }
+}
+function parseExpression(tokens, startIndex) {
+    const stack = [];
+    let i = startIndex;
+    
+    while (i < tokens.length) {
+        const token = tokens[i];
+
+        if (token.type === "number" || token.type === "string" || token.type === "identifier" ) {
+            if (token.type === "string") {
+                token.value = token.value.replace(/"^|"$/g, "");
+            }
+            
+            stack.push({
+                type: token.type === "identifier" ? "variable" : "literal",
+                value: token.value,
+                valueType: token.type
+            });
+            i++;
+        }
+        
+        else if (token.type === "operator") {
+            if (stack.length >= 2) {
+                const right = stack.pop();
+                const left = stack.pop();
+                
+                stack.push({
+                    type: "math",
+                    left: left,
+                    right: right,
+                    operator: token.value
+                });
+            }
+            i++; // unknown token type, skip.
+        } 
+        
+        else if (token.type === "keyword") {
+            break;
+        } else {
+            i++;
+        }
+    } 
+
+    if (stack.length === 1) {
+        return {
+            node: stack[0],
+            consumed: i - startIndex
+        };
+    }
+    return null;
+}
 
 function execute(ast) {
     const context = {};
+    // this is the dictionary with all the variables and their values
 
     const runtime = {
-        print: (node, context) => {
-            let value = node.value;
-
-            if (!node.isLiteral && context[value] !== undefined) {
-                value = context[value];
-            }
-            console.log(value);
-        },
-        let: (node, context) => {
-            context[node.name] = node.value;
-        }
+        print: (node, context) => printFunction(node, context),
+        let: (node, context) => letFunction(node, context)
     };
     for (let node of ast) {
         runtime[node.type]?.(node, context);
         // square brackets allow me to reference a variable when trying to pull a value from an object.
         // the "?." means, only execute this if it exists.
-        }
+    }
+}
+function printFunction(node, context) {
+    const expr = node.expression;
+    if (!expr) {
+        return;
     }
 
+    let value;
+
+    if (expr.type === "math") {
+        value = math(expr, context);
+    } else if (expr.type === "variable") {
+        value = context[expr.value] !== undefined ?
+            context[expr.value] :
+            expr.value;
+    } else {
+        value = expr.value;
+    }
+
+    console.log(value);
+}
+function letFunction(node, context) {
+    const expr = node.expression;
+    if (!expr) {
+        return;
+    }
+
+    let value;
+
+    if (expr.type === "math") {
+        value = math(expr, context);
+    } else if (expr.type === "varible") {
+        value = context[expr.value] !== undefined ?
+            context[expr.value] :
+            expr.value;
+    } else {
+        value = expr.value;
+    }
+    context[node.name] = value;
+}
